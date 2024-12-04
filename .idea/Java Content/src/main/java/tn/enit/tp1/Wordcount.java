5e694bf38e46be7b6e243cdcecd8b2cb8c80a475
Samiimasmoudii <samsoum.tecktonick@gmail.com>
2024-12-04 21:30:14 +0100

    Manip 3 Wordcount java

---
+++ .idea/Java Content/src/main/java/tn/enit/tp1/Wordcount.java
@@ -0,0 +1,35 @@
+package tn.enit.tp1;
+
+import com.google.common.base.Preconditions;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Tuple2;
+
+import java.util.Arrays;
+public class Wordcount {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Wordcount.class);
+
+    public static void main(String[] args) {
+        Preconditions.checkArgument(args.length > 1, "Please provide the path of input file and output dir as parameters.");
+        new Wordcount().run(args[0], args[1]);
+    }
+
+    public void run(String inputFilePath, String outputDir) {
+
+        SparkConf conf = new SparkConf()
+                .setAppName(Wordcount.class.getName());
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> textFile = sc.textFile(inputFilePath);
+        JavaPairRDD<String, Integer> counts = textFile
+                .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((a, b) -> a + b);
+        counts.saveAsTextFile(outputDir);
+    }
+}
